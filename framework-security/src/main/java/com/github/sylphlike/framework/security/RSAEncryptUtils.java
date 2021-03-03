69b4822b061c632e063ccfd9207735ccecc5ce01
Gopal.pan <panlinsen@itcast.cn>
2021-03-03 16:02:04 +0800

    feat(security): RSA支持分段加密

--- framework-security/src/main/java/com/github/sylphlike/framework/security/RSAEncryptUtils.java
+++ framework-security/src/main/java/com/github/sylphlike/framework/security/RSAEncryptUtils.java
@@ -1,62 +1,66 @@
 package com.github.sylphlike.framework.security;
 
-
-import org.apache.commons.codec.binary.Base64;
-
+import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.NoSuchPaddingException;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.security.*;
 import java.security.interfaces.RSAPrivateKey;
 import java.security.interfaces.RSAPublicKey;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
 
 /**
- * RSA非对称加密工具类
- * <p>  time 17:56 2021/01/29  星期五 </p>
+ * RSA 非对称加密工具类
+ * <p>  time 13:58 2021/03/03  星期三 </p>
  * <p> email 15923508369@163.com     </P>
  * @author Gopal.pan
  * @version 1.0.0
  */
-
 public class RSAEncryptUtils {
 
-    public  static final String PUBLIC_KEY       = "public_key";
-    public  static final String PRIVATE_KEY      = "private_key";
-    private static final String KEY_ALGORITHM    = "RSA";
-    private static final String CHAR_SET         = "utf-8";
-    private static final int    KEY_SIZE         = 2048;
+    /** 加密算法RSA */
+    public static final String KEY_ALGORITHM = "RSA";
 
+    /** 签名算法 */
+    public static final String SIGNATURE_ALGORITHM = "MD5withRSA";
 
+    /** 获取公钥的key */
+    private static final String PUBLIC_KEY = "RSAPublicKey";
 
+    /** 获取私钥的key */
+    private static final String PRIVATE_KEY = "RSAPrivateKey";
 
+    /** RSA最大加密明文大小 */
+    private static final int MAX_ENCRYPT_BLOCK = 117;
 
+    /** RSA最大解密密文大小*/
+    private static final int MAX_DECRYPT_BLOCK = 128;
 
 
     /**
-     * 生成RSA公私匙
-     * <p>获取公私 pair.get(RSAEncryptUtils.PUBLIC_KEY) .获取私匙 pair.get(RSAEncryptUtils.PRIVATE_KEY) </p>
-     * <p>  time 18:27 2021/1/29      </p>
+     * 生成密钥对
+     * <p>  time 14:06 2021/3/3       </p>
      * <p> email 15923508369@163.com  </p>
-     * @return  java.util.Map
+     * @return  java.util.Map<java.lang.String,java.lang.Object>
      * @author  Gopal.pan
      */
-    public static Map<String, String> generateRSAPair() {
-        Map<String, String> pair = new HashMap<String, String>();
-        KeyPairGenerator keyPairGen;
-        try {
-            keyPairGen = KeyPairGenerator.getInstance(KEY_ALGORITHM);
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException("get RSA key pair generator failed", e);
-        }
-        keyPairGen.initialize(KEY_SIZE);
+    public static Map<String, Object> genKeyPair() throws Exception {
+        KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance(KEY_ALGORITHM);
+        keyPairGen.initialize(1024);
         KeyPair keyPair = keyPairGen.generateKeyPair();
         RSAPublicKey publicKey = (RSAPublicKey) keyPair.getPublic();
         RSAPrivateKey privateKey = (RSAPrivateKey) keyPair.getPrivate();
-        pair.put(PUBLIC_KEY, Base64.encodeBase64String(publicKey.getEncoded()));
-        pair.put(PRIVATE_KEY, Base64.encodeBase64String(privateKey.getEncoded()));
-        return pair;
+        Map<String, Object> keyMap = new HashMap<>(2);
+        keyMap.put(PUBLIC_KEY, publicKey);
+        keyMap.put(PRIVATE_KEY, privateKey);
+        return keyMap;
     }
 
 
@@ -64,76 +68,188 @@ public class RSAEncryptUtils {
 
 
     /**
-     * 公匙加密
-     * <p>  time 18:27 2021/1/29      </p>
+     * 用私钥对信息生成数字签名
+     * <p>  time 14:07 2021/3/3       </p>
      * <p> email 15923508369@163.com  </p>
-     * @param data  待加密数据
-     * @param key   公钥
+     * @param data       已加密数据字节数组
+     * @param privateKey 私钥(BASE64编码)
      * @return  java.lang.String
      * @throws  Exception ex
      * @author  Gopal.pan
      */
-    public static String encryptByPublicKey(String data, String key) throws Exception {
-
-        X509EncodedKeySpec x509KeySpec = new X509EncodedKeySpec(Base64.decodeBase64(key));
+    public static String sign(String data, String privateKey) throws Exception {
+        byte[] keyBytes = Base64.getDecoder().decode(privateKey);
+        PKCS8EncodedKeySpec pkcs8KeySpec = new PKCS8EncodedKeySpec(keyBytes);
         KeyFactory keyFactory = KeyFactory.getInstance(KEY_ALGORITHM);
-        PublicKey publicKey = keyFactory.generatePublic(x509KeySpec);
-        Cipher cipher = Cipher.getInstance(keyFactory.getAlgorithm());
-        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
-        byte[] dateByte = cipher.doFinal(data.getBytes(CHAR_SET));
-
-        return Base64.encodeBase64String(dateByte);
+        PrivateKey privateK = keyFactory.generatePrivate(pkcs8KeySpec);
+        Signature signature = Signature.getInstance(SIGNATURE_ALGORITHM);
+        signature.initSign(privateK);
+        signature.update(data.getBytes(StandardCharsets.UTF_8));
+        return Base64.getEncoder().encodeToString(signature.sign());
     }
+
+
+
+    /**
+     * 校验数字签名
+     * <p>  time 14:07 2021/3/3       </p>
+     * <p> email 15923508369@163.com  </p>
+     * @param data      已加密数据
+     * @param publicKey 公钥(BASE64编码)
+     * @param sign      数字签名
+     * @return  boolean
+     * @throws  Exception ex
+     * @author  Gopal.pan
+     */
+    public static boolean verify(String data, String publicKey, String sign) throws Exception {
+        byte[] keyBytes = Base64.getDecoder().decode(publicKey);
+        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(keyBytes);
+        KeyFactory keyFactory = KeyFactory.getInstance(KEY_ALGORITHM);
+        PublicKey publicK = keyFactory.generatePublic(keySpec);
+        Signature signature = Signature.getInstance(SIGNATURE_ALGORITHM);
+        signature.initVerify(publicK);
+        signature.update(data.getBytes(StandardCharsets.UTF_8));
+        return signature.verify(Base64.getDecoder().decode(sign));
+    }
+
 
 
 
 
     /**
-     * 公匙解密
-     * <p>  time 18:27 2021/1/29      </p>
+     * 公钥加密
+     * <p>  time 14:19 2021/3/3       </p>
      * <p> email 15923508369@163.com  </p>
-     * @param data  业务数据
-     * @param key   公钥
-     * @return  java.lang.String
+     * @param data      源数据
+     * @param publicKey 公钥(BASE64编码)
+     * @return  java.lang.String  Base64编码的字符串
      * @throws  Exception ex
      * @author  Gopal.pan
      */
-    public static String decryptByPublicKey(String data, String key) throws Exception {
-
-        X509EncodedKeySpec x509KeySpec = new X509EncodedKeySpec( Base64.decodeBase64(key));
-        KeyFactory keyFactory = KeyFactory.getInstance(KEY_ALGORITHM);
-        PublicKey publicKey = keyFactory.generatePublic(x509KeySpec);
-        Cipher cipher = Cipher.getInstance(keyFactory.getAlgorithm());
-        cipher.init(Cipher.DECRYPT_MODE, publicKey);
-
-        return new String(cipher.doFinal(Base64.decodeBase64(data)));
+    public static String encryptByPublicKey(String data,String publicKey) throws Exception {
+        byte[] bytes = encryptByPublicKey(data.getBytes(StandardCharsets.UTF_8), publicKey);
+        return Base64.getEncoder().encodeToString(bytes);
 
     }
 
 
+    /**
+     * 公钥加密
+     * <p>  time 14:08 2021/3/3       </p>
+     * <p> email 15923508369@163.com  </p>
+     * @param data      源数据
+     * @param publicKey 公钥(BASE64编码)
+     * @return  byte[]  为编码的字节数组
+     * @throws  Exception  ex
+     * @author  Gopal.pan
+     */
+    public static byte[] encryptByPublicKey(byte[] data, String publicKey) throws Exception {
+        byte[] keyBytes = Base64.getDecoder().decode(publicKey);
+        X509EncodedKeySpec x509KeySpec = new X509EncodedKeySpec(keyBytes);
+        KeyFactory keyFactory = KeyFactory.getInstance(KEY_ALGORITHM);
+        Key publicK = keyFactory.generatePublic(x509KeySpec);
+        // 对数据加密
+        return getBytes(data, keyFactory, publicK, Cipher.ENCRYPT_MODE, MAX_ENCRYPT_BLOCK);
+    }
 
-
+    private static byte[] getBytes(byte[] data, KeyFactory keyFactory, Key publicK, int encryptMode, int maxEncryptBlock) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, IOException {
+        Cipher cipher = getCipher(keyFactory, publicK, encryptMode);
+        int inputLen = data.length;
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        int offSet = 0;
+        byte[] cache;
+        int i = 0;
+        // 对数据分段加密
+        while (inputLen - offSet > 0) {
+            if (inputLen - offSet > maxEncryptBlock) {
+                cache = cipher.doFinal(data, offSet, maxEncryptBlock);
+            } else {
+                cache = cipher.doFinal(data, offSet, inputLen - offSet);
+            }
+            out.write(cache, 0, cache.length);
+            i++;
+            offSet = i * maxEncryptBlock;
+        }
+        byte[] encryptedData = out.toByteArray();
+        out.close();
+        return encryptedData;
+    }
 
 
     /**
      * 私钥加密
-     * <p>  time 18:27 2021/1/29      </p>
+     * <p>  time 14:37 2021/3/3       </p>
      * <p> email 15923508369@163.com  </p>
-     * @param data  业务数据
-     * @param key   私钥
+     * @param data       源数据
+     * @param privateKey 私钥(BASE64编码)
      * @return  java.lang.String
      * @throws  Exception ex
      * @author  Gopal.pan
      */
-    public static String encryptByPrivateKey(String data, String key) throws Exception {
+    public static String encryptByPrivateKey(String data,String privateKey) throws Exception {
+        byte[] bytes = encryptByPrivateKey(data.getBytes(StandardCharsets.UTF_8), privateKey);
+        return Base64.getEncoder().encodeToString(bytes);
+    }
 
-        PKCS8EncodedKeySpec pkcs8KeySpec = new PKCS8EncodedKeySpec(Base64.decodeBase64(key));
+
+    /**
+     * 私钥加密
+     * <p>  time 14:09 2021/3/3       </p>
+     * <p> email 15923508369@163.com  </p>
+     * @param data       源数据
+     * @param privateKey 私钥(BASE64编码)
+     * @return  byte[]
+     * @throws  Exception ex
+     * @author  Gopal.pan
+     */
+    public static byte[] encryptByPrivateKey(byte[] data, String privateKey) throws Exception {
+        byte[] keyBytes = Base64.getDecoder().decode(privateKey);
+        PKCS8EncodedKeySpec pkcs8KeySpec = new PKCS8EncodedKeySpec(keyBytes);
         KeyFactory keyFactory = KeyFactory.getInstance(KEY_ALGORITHM);
-        PrivateKey privateKey = keyFactory.generatePrivate(pkcs8KeySpec);
-        Cipher cipher = Cipher.getInstance(keyFactory.getAlgorithm());
-        cipher.init(Cipher.ENCRYPT_MODE, privateKey);
+        Key privateK = keyFactory.generatePrivate(pkcs8KeySpec);
+        return getBytes(data, keyFactory, privateK, Cipher.ENCRYPT_MODE, MAX_ENCRYPT_BLOCK);
+    }
 
-        return Base64.encodeBase64String(cipher.doFinal(data.getBytes()));
+    private static Cipher getCipher(KeyFactory keyFactory, Key privateK, int encryptMode) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException {
+        Cipher cipher = Cipher.getInstance(keyFactory.getAlgorithm());
+        cipher.init(encryptMode, privateK);
+        return cipher;
+    }
+
+
+    /**
+     * 公钥解密
+     * <p>  time 14:40 2021/3/3       </p>
+     * <p> email 15923508369@163.com  </p>
+     * @param data       已加密数据
+     * @param publicKey  公钥(BASE64编码)
+     * @return  java.lang.String
+     * @throws  Exception ex
+     * @author  Gopal.pan
+     */
+    public static String decryptByPublicKey(String data,String publicKey) throws Exception {
+        byte[] bytes = decryptByPublicKey(Base64.getDecoder().decode(data), publicKey);
+        return new String(bytes);
+
+    }
+
+
+    /**
+     * 公钥解密
+     * <p>  time 14:08 2021/3/3       </p>
+     * <p> email 15923508369@163.com  </p>
+     * @param encryptedData 已加密数据
+     * @param publicKey     公钥(BASE64编码)
+     * @return  byte[]
+     * @throws  Exception ex
+     * @author  Gopal.pan
+     */
+    public static byte[] decryptByPublicKey(byte[] encryptedData, String publicKey) throws Exception {
+        byte[] keyBytes = Base64.getDecoder().decode(publicKey);
+        X509EncodedKeySpec x509KeySpec = new X509EncodedKeySpec(keyBytes);
+        KeyFactory keyFactory = KeyFactory.getInstance(KEY_ALGORITHM);
+        Key publicK = keyFactory.generatePublic(x509KeySpec);
+        return getBytes(encryptedData, keyFactory, publicK, Cipher.DECRYPT_MODE, MAX_DECRYPT_BLOCK);
     }
 
 
@@ -141,25 +257,74 @@ public class RSAEncryptUtils {
 
 
     /**
-     * 私匙解密
-     * <p>  time 18:27 2021/1/29      </p>
+     * 私钥解密
+     * <p>  time 14:31 2021/3/3       </p>
      * <p> email 15923508369@163.com  </p>
-     * @param data 业务数据
-     * @param key  私钥
+     * @param data          已加密数据
+     * @param privateKey    私钥(BASE64编码)
      * @return  java.lang.String
      * @throws  Exception ex
      * @author  Gopal.pan
      */
-    public static String decryptByPrivateKey(String data, String key) throws Exception {
+    public static String decryptByPrivateKey(String data,String privateKey) throws Exception {
+        byte[] bytes = RSAEncryptUtils.decryptByPrivateKey(Base64.getDecoder().decode(data), privateKey);
+        return new String(bytes);
 
-        PKCS8EncodedKeySpec pkcs8EncodedKeySpec = new PKCS8EncodedKeySpec(Base64.decodeBase64(key));
-        KeyFactory keyFactory = KeyFactory.getInstance(KEY_ALGORITHM);
-        PrivateKey privateKey = keyFactory.generatePrivate(pkcs8EncodedKeySpec);
-        Cipher cipher = Cipher.getInstance(keyFactory.getAlgorithm());
-        cipher.init(Cipher.DECRYPT_MODE, privateKey);
-        byte[] dataByte = cipher.doFinal(Base64.decodeBase64(data));
-
-        return new String(dataByte);
     }
+
+    /**
+     * 私钥解密
+     * <p>  time 14:07 2021/3/3       </p>
+     * <p> email 15923508369@163.com  </p>
+     * @param encryptedData 已加密数据
+     * @param privateKey    私钥(BASE64编码)
+     * @return  byte[]
+     * @throws  Exception ex
+     * @author  Gopal.pan
+     */
+    public static byte[] decryptByPrivateKey(byte[] encryptedData, String privateKey) throws Exception {
+        byte[] keyBytes = Base64.getDecoder().decode(privateKey);
+        PKCS8EncodedKeySpec pkcs8KeySpec = new PKCS8EncodedKeySpec(keyBytes);
+        KeyFactory keyFactory = KeyFactory.getInstance(KEY_ALGORITHM);
+        Key privateK = keyFactory.generatePrivate(pkcs8KeySpec);
+        return getBytes(encryptedData, keyFactory, privateK, Cipher.DECRYPT_MODE, MAX_DECRYPT_BLOCK);
+    }
+
+
+
+
+
+    /**
+     * 获取私钥
+     * <p>  time 14:09 2021/3/3       </p>
+     * <p> email 15923508369@163.com  </p>
+     * @param keyMap 密钥对
+     * @return  java.lang.String
+     * @throws  Exception ex
+     * @author  Gopal.pan
+     */
+    public static String getPrivateKey(Map<String, Object> keyMap) throws Exception {
+        Key key = (Key) keyMap.get(PRIVATE_KEY);
+        return Base64.getEncoder().encodeToString(key.getEncoded());
+    }
+
+
+
+    /**
+     * 获取公钥
+     * <p>  time 14:10 2021/3/3       </p>
+     * <p> email 15923508369@163.com  </p>
+     * @param keyMap 密钥对
+     * @return  java.lang.String
+     * @throws  Exception ex
+     * @author  Gopal.pan
+     */
+    public static String getPublicKey(Map<String, Object> keyMap) throws Exception {
+        Key key = (Key) keyMap.get(PUBLIC_KEY);
+        return Base64.getEncoder().encodeToString(key.getEncoded());
+    }
+
+
+
 
 }
